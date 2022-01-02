751f0054cc895046bd3a5fabbfbfb1ad36fbf002
elcaskerito <idimhtt4t@gmail.com>
2022-01-02 11:12:08 +0100

    update

--- es.py
+++ es.py
@@ -9,5 +9,7 @@ dat=cgi.FieldStorage()
 nom=dat.getvalue("a")
 prenom=dat.getvalue("b")
 
+y2meta.com-Python Database(mysql) connection Using XAMPP with HTML-Form-(1080p)
+
 # print("<p>nom : ",nom,"</p><br>")
 # print("<p>Prenom : ",prenom,"</p>")
